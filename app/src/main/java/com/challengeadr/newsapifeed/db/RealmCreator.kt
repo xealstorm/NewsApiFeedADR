5be49102c7f0c13e4192cf6475acbdc8fe8aa1a8
Alex <xeal@Alex-MacBook-Pro.local>
2020-03-19 15:39:09 +0100

    ABR-104-Integrate Realm

---
+++ app/src/main/java/com/challengeadr/newsapifeed/db/RealmCreator.kt
@@ -0,0 +1,29 @@
+package com.challengeadr.newsapifeed.db
+
+import android.content.Context
+import com.facebook.stetho.Stetho
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+object RealmCreator {
+    val REALM_FILE_NAME = "db.realm"
+    fun create(context: Context): Realm {
+        Stetho.initialize(
+            Stetho.newInitializerBuilder(context)
+                .enableDumpapp(Stetho.defaultDumperPluginsProvider(context))
+                .enableWebKitInspector(
+                    RealmInspectorModulesProvider.builder(context)
+                        .withMetaTables()
+                        .build()
+                )
+                .build()
+        )
+        Realm.init(context)
+        val configuration = RealmConfiguration.Builder()
+            .name(REALM_FILE_NAME)
+            .schemaVersion(1)
+            .build()
+        return Realm.getInstance(configuration)
+    }
+}
--- app/src/main/java/com/challengeadr/newsapifeed/di/app/AppModule.kt
+++ app/src/main/java/com/challengeadr/newsapifeed/di/app/AppModule.kt
@@ -1,13 +1,21 @@
 package com.challengeadr.newsapifeed.di.app
 
 import com.challengeadr.newsapifeed.App
+import com.challengeadr.newsapifeed.db.RealmCreator
 import com.challengeadr.newsapifeed.util.scedulers.AppSchedulerProvider
 import dagger.Module
 import dagger.Provides
+import io.realm.Realm
 import javax.inject.Singleton
 
 @Module
 class AppModule(private val app: App) {
+    @Provides
+    @Singleton
+    fun provideRealm(app: App): Realm {
+        return RealmCreator.create(app.applicationContext)
+    }
+
     @Provides
     @Singleton
     fun provideSchedulerProvider() = AppSchedulerProvider()
